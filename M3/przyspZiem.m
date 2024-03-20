38f08292ac24f7bf03c035a8ca5924cffa0d89a3
Błażej Drozd <drozdbazej@gmail.com>
2024-03-20 15:29:00 +0100

    Dodano wynik g

--- M3/przyspZiem.m
+++ M3/przyspZiem.m
@@ -1,8 +1,23 @@
 % Wyznaczanie przyspieszenia ziemskiego za pomocą wahadła matematycznego.
 clc, clearvars
 
-l1 = 10; % cm
-l2 = 110; % cm
+% Górne położenie wahadła
+l1 = 10 * 10^-2; % m
 
-T1 = [43.43 43.41 42.97 43.81 43.12]; % s
-T2 = [57.78 58.13 58.12 58.43 57.69]; % s
+% Dolne położenie wahadła
+l2 = 110 * 10^-2; % m
+
+% Okres 20 drgań w górnym położeniu wahadła
+T1 = [43.43 43.41 42.97 43.81 43.12] / 20; % s
+
+% Okres 20 drgań w dolnym położeniu wahadła
+T2 = [57.78 58.13 58.12 58.43 57.69] / 20; % s
+
+g_wyniki = 1:5;
+
+for i = 1:5
+    g_wyniki(i) = f_g(l1, l2, T2(i), T1(i));
+end
+
+g_srednia = mean(g_wyniki);
+% ok. 10.612942260169252
