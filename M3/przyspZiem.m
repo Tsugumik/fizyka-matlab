618553ebf10054dd27384b900eed22b9cf9062ca
Błażej Drozd <drozdbazej@gmail.com>
2024-03-20 11:23:45 +0100

    Dodano wyniki pomiarów T1 oraz T2

--- M3/przyspZiem.m
+++ M3/przyspZiem.m
@@ -1,2 +1,8 @@
 % Wyznaczanie przyspieszenia ziemskiego za pomocą wahadła matematycznego.
 clc, clearvars
+
+l1 = 10; % cm
+l2 = 110; % cm
+
+T1 = [43.43 43.41 42.97 43.81 43.12]; % s
+T2 = [57.78 58.13 58.12 58.43 57.69]; % s
