7aa09df6f1f64ad9d2c4bb58729f2066dcfbd99f
Błażej Drozd <drozdbazej@gmail.com>
2024-03-20 15:28:20 +0100

    Update f_g.m

--- M3/f_g.m
+++ M3/f_g.m
@@ -1,5 +1,4 @@
-% Funkcja wyliczająca przyspieszenie ziemskie
-function g = f_g(l1,l2,T2_srednia,T1_srednia)
-    g = ((4*(pi^2))*(l2-l1))/((T2_srednia^2)-(T1_srednia^2));
+% Funkcja licząca przyspieszenie ziemskie
+function g = f_g(l1, l2, T2, T1)
+    g = ((4*(pi^2))*(l2-l1))/((T2^2)-(T1^2));
 end
-
