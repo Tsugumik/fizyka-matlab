1ce95172afffe186400ec1b1dadea8c937788849
Błażej Drozd <drozdbazej@gmail.com>
2024-03-20 15:26:58 +0100

    Update f_ua.m

--- f_ua.m
+++ f_ua.m
@@ -1,6 +1,7 @@
 % Funkcja licząca niepewność typu a
 function w = f_ua(macierz_wynikow)
+    n = length(macierz_wynikow);
     srednia = mean(macierz_wynikow);
-    w = sqrt(sum((macierz_wynikow-srednia).^2)/(3*(3-1)));
+    w = sqrt(sum((macierz_wynikow-srednia).^2)/(n*(n-1)));
 end
 
