fe7cbadd6a93f4e1a6d536dd037d4d134e64bb1d
Błażej Drozd <drozdbazej@gmail.com>
2024-03-25 15:34:17 +0100

    Inaczej liczone g

--- M3/przyspZiem.m
+++ M3/przyspZiem.m
@@ -10,16 +10,13 @@ l2 = 110 * 10^-2; % m
 % Okres 20 drgań w górnym położeniu wahadła
 T1 = [43.43 43.41 42.97 43.81 43.12]; % s
 T1_okresy = T1/20;
+T1_okresy_srednia = mean(T1_okresy);
 
 % Okres 20 drgań w dolnym położeniu wahadła
 T2 = [57.78 58.13 58.12 58.43 57.69]; % s
 T2_okresy = T2/20;
+T2_okresy_srednia = mean(T2_okresy);
 
-g_wyniki = 1:5;
+g = f_g(l1, l2, T2_okresy_srednia, T1_okresy_srednia);
+% ok. 10.6094
 
-for i = 1:5
-    g_wyniki(i) = f_g(l1, l2, T2_okresy(i), T1_okresy(i));
-end
-
-g_srednia = mean(g_wyniki);
-% ok. 10.612942260169252
--- f_ua.m
+++ f_ua.m
@@ -4,4 +4,3 @@ function w = f_ua(macierz_wynikow)
     srednia = mean(macierz_wynikow);
     w = sqrt(sum((macierz_wynikow-srednia).^2)/(n*(n-1)));
 end
-
