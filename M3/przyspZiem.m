df86e000401e2a9a1a957353bc793a03543e3290
Błażej Drozd <drozdbazej@gmail.com>
2024-03-25 17:28:23 +0100

    Wyliczenie niepewności i podanie wyników

--- M3/przyspZiem.m
+++ M3/przyspZiem.m
@@ -20,3 +20,31 @@ T2_okresy_srednia = mean(T2_okresy);
 g = f_g(l1, l2, T2_okresy_srednia, T1_okresy_srednia);
 % ok. 10.6094
 
+% -- Niepewności --
+d_l = 0.1 * 10^-2; % m
+d_T = 0.2/20; % Czyli niepewność czasu dt = 0.2 przez 20 okresów
+
+% dla l1 oraz l2
+uc_l1 = f_ub(d_l);
+uc_l2 = uc_l1;
+
+% dla T1
+ub_t1 = f_ub(d_T);
+uc_t1 = f_uc(T1_okresy, d_T);
+
+% dla T2
+uc_t2 = f_uc(T2_okresy, d_T);
+
+% -- Pochodne cząstkowe
+pochodna_l1 = (4*(pi^2))/((T2_okresy_srednia^2)-(T1_okresy_srednia^2));
+pochodna_l2 = pochodna_l1;
+pochodna_t1 = (8*(pi^2)*T1_okresy_srednia*(l2-l1))/(((T2_okresy_srednia^2)-(T1_okresy_srednia^2))^2);
+pochodna_t2 = (8*(pi^2)*T2_okresy_srednia*(l2-l1))/(((T2_okresy_srednia^2)-(T1_okresy_srednia^2))^2);
+
+% Niepewność złożona
+suma = (pochodna_l1^2)*(uc_l1^2)+(pochodna_l2^2)*(uc_l2^2)+(pochodna_t1^2)*(uc_t1^2)+(pochodna_t2^2)*(uc_t2^2);
+uc_g = sqrt(suma);
+uc_g2 = 2*uc_g;
+
+% Wynik końcowy
+% g = 10.609 +- 0.371
